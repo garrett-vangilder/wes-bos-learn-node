eda4a587456f3386e10b75ac3f147fdaf2b6133c
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-07-19 15:31:40 -0400

    creates unique slug

--- store-application/models/Store.js
+++ store-application/models/Store.js
@@ -35,12 +35,18 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stops this function from running
   }
   this.slug = slug(this.name);
+  // find other similar stores garrett, garrett-1, garrett-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
 });
 
