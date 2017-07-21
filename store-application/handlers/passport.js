a3eb7acb80d2290bd8e743a3938bf57e1355c7dd
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-07-21 09:14:59 -0400

    login in functionality

--- store-application/handlers/passport.js
+++ store-application/handlers/passport.js
@@ -3,3 +3,6 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
