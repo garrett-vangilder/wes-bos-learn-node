e4534e5046be9ab174edd33542bb2411d46d17da
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-07-25 19:42:34 -0400

    adds user hearted stores

--- store-application/public/javascripts/delicious-app.js
+++ store-application/public/javascripts/delicious-app.js
@@ -4,7 +4,10 @@ import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
+import ajaxHeart from './modules/heart';
 
 autocomplete( $('#address'), $('#lat'), $('#lng') );
 typeAhead( $('.search') );
 makeMap( $('#map') );
+const heartForm = $$('form.heart');
+heartForm.on('submit', ajaxHeart);
--- store-application/routes/index.js
+++ store-application/routes/index.js
@@ -45,11 +45,14 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+
 
 // API
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 
 module.exports = router;
